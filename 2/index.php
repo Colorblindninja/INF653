52061386b09d9a17f5ef9eedea4d152628565c28
Zach Lamoreaux <zachlamoreaux@hotmail.com>
2022-01-28 14:50:05 -0800

    cleaned up week 2 project

--- 2/index.php
+++ 2/index.php
@@ -1,12 +1,9 @@
-
-
-
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Project 2</title>
+    <title>Week 2 Project</title>
     <link rel="stylesheet" href="css/main.css">
   </head>
   <body>
